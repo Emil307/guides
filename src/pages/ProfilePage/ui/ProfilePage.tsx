395a15ed750777a3ceeaa3051729c92a6374979f
Emil307 <emilnovikov392@gmail.com>
2025-02-13 01:10:34 +0600

    feat: navbar

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { ChangeLanguage } from "widgets/ChangeLanguage";
+import { ToggleTheme } from "widgets/ToggleTheme";
 
 const ProfilePage: React.FC = () => {
   const { t } = useTranslation("profile");
 
-  return <div>{t("Profile page")}</div>;
+  return (
+    <div>
+      <ToggleTheme />
+      <ChangeLanguage />
+      {t("Profile page")}
+    </div>
+  );
 };
 
 export default ProfilePage;
--- src/widgets/NavigationMenu/ui/NavigationMenu.tsx
+++ src/widgets/NavigationMenu/ui/NavigationMenu.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import styles from "./styles.module.scss";
 import clsx from "clsx";
 import { Link } from "react-router-dom";
+import HomeWhiteIcon from "shared/assets/icons/home-white.svg";
+import HomeBlackIcon from "shared/assets/icons/home-black.svg";
+import CreateWhiteIcon from "shared/assets/icons/create-white.svg";
+import CreateBlackIcon from "shared/assets/icons/create-black.svg";
+import ProfileWhiteIcon from "shared/assets/icons/profile-white.svg";
+import ProfileBlackIcon from "shared/assets/icons/profile-black.svg";
+import { useTheme } from "shared/config";
+import { useTranslation } from "react-i18next";
 
 interface NavigationMenuProps {
   className?: string;
@@ -10,11 +18,26 @@ interface NavigationMenuProps {
 export const NavigationMenu: React.FC<NavigationMenuProps> = ({
   className,
 }) => {
+  const { theme } = useTheme();
+  const { t } = useTranslation("navbar");
+
   return (
     <div className={clsx(styles.navigationMenu, className)}>
-      <Link to="/">Главная</Link>
-      <Link to="/create">Создать</Link>
-      <Link to="/profile">Профиль</Link>
+      <Link to="/" className={styles.link}>
+        {theme === "light" && <HomeBlackIcon height={24} />}
+        {theme === "dark" && <HomeWhiteIcon height={24} />}
+        {t("Home")}
+      </Link>
+      <Link to="/create" className={styles.link}>
+        {theme === "light" && <CreateBlackIcon height={24} />}
+        {theme === "dark" && <CreateWhiteIcon height={24} />}
+        {t("Create")}
+      </Link>
+      <Link to="/profile" className={styles.link}>
+        {theme === "light" && <ProfileBlackIcon height={24} />}
+        {theme === "dark" && <ProfileWhiteIcon height={24} />}
+        {t("Profile")}
+      </Link>
     </div>
   );
 };
