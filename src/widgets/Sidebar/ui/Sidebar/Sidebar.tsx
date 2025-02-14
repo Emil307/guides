cec2092eaee8627c7dd7acdd365e0961dd047299
Emil307 <emilnovikov392@gmail.com>
2025-02-14 12:21:32 +0600

    wip: layout sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -6,6 +6,9 @@ import sidebarState from "widgets/Sidebar/store/sidebarState";
 import { ToggleTheme } from "widgets/ToggleTheme";
 import { ChangeLanguage } from "widgets/ChangeLanguage";
 import { observer } from "mobx-react-lite";
+import CloseIcon from "shared/assets/icons/close.svg";
+import LanguageIcon from "shared/assets/icons/lang.svg";
+import { useTheme } from "shared/config";
 
 interface SidebarProps {
   className?: string;
@@ -13,6 +16,7 @@ interface SidebarProps {
 
 export const Sidebar: React.FC<SidebarProps> = observer(({ className }) => {
   const { t } = useTranslation("translation");
+  const { theme } = useTheme();
 
   return (
     <div
@@ -27,8 +31,32 @@ export const Sidebar: React.FC<SidebarProps> = observer(({ className }) => {
           [styles["content-collapsed"]]: sidebarState.isCollapsed,
         })}
       >
-        <ToggleTheme />
-        <ChangeLanguage />
+        <div className={styles.top}>
+          <h2 className={styles.title}>Настройки</h2>
+          <button onClick={() => sidebarState.setIsCollapsed(true)}>
+            <CloseIcon />
+          </button>
+        </div>
+        <div className={styles.settings}>
+          <div className={styles.langWrapper}>
+            <span className={styles.lang}>
+              <LanguageIcon />
+              Язык
+            </span>
+            <ChangeLanguage />
+          </div>
+          <div className={styles.themeWrapper}>
+            <span className={styles.theme}>
+              {theme === "dark" && <>Темная тема</>}
+              {theme === "light" && <>Светлая тема</>}
+            </span>
+            <ToggleTheme />
+          </div>
+        </div>
+        <div className={styles.credits}>
+          <p>Политика конфиденциальности</p>
+          <p>Все права защищены</p>
+        </div>
       </div>
     </div>
   );
