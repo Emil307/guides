d77233f9c3ce707eb44011dc0bbb17acff1d9409
Emil307 <emilnovikov392@gmail.com>
2025-02-14 13:25:15 +0600

    layout: profile

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Balance } from "widgets/Balance";
+import { ProfileInfo } from "widgets/ProfileInfo";
 import { Sidebar } from "widgets/Sidebar";
 
 const ProfilePage: React.FC = () => {
   return (
     <div>
       <Balance />
+      <ProfileInfo />
       <Sidebar />
     </div>
   );
---
+++ src/widgets/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+export const ProfileInfo: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.avatar}
+        style={{
+          backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Felis_silvestris_silvestris.jpg/550px-Felis_silvestris_silvestris.jpg)`,
+        }}
+      />
+      <div className={styles.right}>
+        <div className={styles.top}>
+          <h2 className={styles.name}>Ксения Гладкова</h2>
+        </div>
+        <p className={styles.desc}>
+          Преподаю химию и биологию, сюда пишу свои короткие статьи и гайды
+        </p>
+        <span className={styles.link}>https//t.me/ksustudy.com</span>
+      </div>
+    </div>
+  );
+};
